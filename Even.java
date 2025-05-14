18de56ce077ac8cebe9d14d14e429b23b9282572
THIPPESWAMY J <Thippesh1996@gmail.com>
2025-05-14 10:43:29 +0530

    Even number program

--- Even.java
+++ Even.java
@@ -1,1 +1,13 @@
-public class Even{}
+public class Even{
+	public static void main(String [] args){
+		int n=2;
+		
+		 if(n&1=0){
+			 System.out.println("The given number is even");
+			 
+		 }
+		 else {
+			 System.out.println("The given number is odd");
+		 }
+	}
+}
