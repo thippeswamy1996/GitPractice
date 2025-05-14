d6468e3169e4262a0f7682f0f89043a09cb36c0d
THIPPESWAMY J <Thippesh1996@gmail.com>
2025-05-14 08:04:11 +0530

    Git second commit

--- AB.java
+++ AB.java
@@ -2,10 +2,12 @@ public class AB{
 public static void main(String [] args){
     int a=10;
     int b=20;
-    int temp ;
+    int temp  ;
     temp=a;
       a=b;
     b=temp;
-    System.out.println(+a""+""+b);
+   // System.out.println(+a""+""+b);
+    System.out.println(a); 
+	System.out.println(b);
 }
 }
